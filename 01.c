fe60bd4b862e4eedb63c91fdf9a673990cc67453
Shankar141005 <shankar141005@gmail.com>
2025-01-30 11:33:32 +0530

    done

---
+++ 01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    printf("hello world\n");
+    int year=2025;
+    float parice=3.4;
+    char letter='a';
+    char name[15]="shankar \n";
+    printf("%d",year);
+    printf ("%f""\n",parice);
+    printf("%s""\n",letter);
+    return 0;    
+}
+
+    
+
+
